6f270ceb612cb253259369ddcad20d1dcc83151f
Peter Abah <peterabah.ob@gmail.com>
2022-04-07 02:21:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Aedi Notes
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="m-4">
+      <h1 className="text-green-700">Aedi Notes!</h1>
+    </div>
+  );
+}
+
+export default App;
