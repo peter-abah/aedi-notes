1cfbe98f6dee0ef8198efb3a1b62d64cb853aae5
Peter Abah <peterabah.ob@gmail.com>
2022-04-07 02:43:03 +0100

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
+  const foo = (
     <div className="m-4">
       <h1 className="text-green-700">Aedi Notes!</h1>
     </div>
   );
+  
+  return (
+    <Routes>
+      <Route path='/' element={foo} />
+    </Routes>
+  )
 }
 
 export default App;
