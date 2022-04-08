103af0fb19c11eeb9f921f74cd3456622fa7f45a
Peter Abah <peterabah.ob@gmail.com>
2022-04-08 20:12:24 +0100

    Add SideBar styles

--- src/components/SideBar/Collections.tsx
+++ src/components/SideBar/Collections.tsx
@@ -2,22 +2,29 @@ import { useBoolean } from 'usehooks-ts'
 import { useCollections } from '../../contexts/CollectionsContext';
 import { Link } from 'react-router-dom';
 
+import { MdFolder, MdCreateNewFolder } from 'react-icons/md';
+
 const Collections = () => {
   const { value: isOpen, toggle } = useBoolean(true);
   const { collections } = useCollections();
   
   return (
-    <section>
-      <h2 className='font-bold'>
-        <button onClick={toggle}>Collections</button>
+    <section className='mb-6'>
+      <h2 className='mb-2'>
+        <button className='font-bold' onClick={toggle}>Collections</button>
       </h2>
 
       {isOpen && 
-        <div>
+        <div className='flex flex-col gap-2 text-sm'>
           {collections.map(({ name, id }) => (
-            <Link to={`/collections/${id}`} key={id}>{name}</Link>
+            <Link className='flex items-center gap-2' to={`/collections/${id}`} key={id}>
+              <MdFolder /> {name}
+            </Link>
           ))}
-          <Link to='/collections/new'>Add collection</Link>
+
+          <Link className='flex items-center gap-2' to='/collections/new'>
+            <MdCreateNewFolder /> Add collection
+          </Link>
         </div>
       }
     </section>
--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -9,15 +9,15 @@ interface Props {
 
 const SideBar = ({ isOpen, toggle, setIsOpen }: Props) => {
   return ( isOpen ?
-    <nav className='fixed top-0 left-0 p-4 h-screen overflow-y-auto bg-secondary'>
-      <section className='mb-4 flex flex-col gap-2'>
+    <nav className='fixed top-0 left-0 p-8 h-screen w-full max-w-xs overflow-y-auto bg-secondary'>
+      <section className='mb-6 flex flex-col gap-2'>
         <Link to='/'>Notes</Link>
         <Link to='/notes/recent'>Recent</Link>
       </section>
       
       <Collections />
       
-      <section className='mb-4 flex flex-col gap-2'>
+      <section className='mb-6 flex flex-col gap-2'>
         <Link to='/trash'>Trash</Link>
         <Link to='/settings'>Settings</Link>
       </section>
