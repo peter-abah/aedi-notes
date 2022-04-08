010d78bdc9e0aecb3b09397d2569f372bf94b3b2
Peter Abah <peterabah.ob@gmail.com>
2022-04-08 21:23:40 +0100

    Add close btn and close on click outside to Sidebar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,4 +1,7 @@
+import { useRef } from 'react';
+import { useOnClickOutside } from 'usehooks-ts';
 import { Link } from 'react-router-dom';
+import { MdClose } from 'react-icons/md';
 import Collections from './Collections';
 
 interface Props {
@@ -8,20 +11,33 @@ interface Props {
 };
 
 const SideBar = ({ isOpen, toggle, setIsOpen }: Props) => {
+  const closeNav = () => setIsOpen(false);
+
+  const ref = useRef(null);
+  useOnClickOutside(ref, closeNav)
+
   return ( isOpen ?
-    <nav className='fixed top-0 left-0 p-8 h-screen w-full max-w-xs overflow-y-auto bg-secondary'>
-      <section className='mb-6 flex flex-col gap-2'>
-        <Link to='/'>Notes</Link>
-        <Link to='/notes/recent'>Recent</Link>
-      </section>
-      
-      <Collections />
-      
-      <section className='mb-6 flex flex-col gap-2'>
-        <Link to='/trash'>Trash</Link>
-        <Link to='/settings'>Settings</Link>
-      </section>
-    </nav> : null
+    <div className='fixed top-0 left-0 h-screen w-screen bg-secondary/30'>
+      <nav ref={ref} className='h-full w-full max-w-xs p-6 overflow-y-auto bg-secondary'>
+        <div className='pb-4 flex flex-end'>
+          <button onClick={closeNav}>
+            <MdClose className='text-lg' />
+          </button>
+        </div>
+
+        <section className='mb-6 flex flex-col gap-2'>
+          <Link to='/'>Notes</Link>
+          <Link to='/notes/recent'>Recent</Link>
+        </section>
+        
+        <Collections />
+        
+        <section className='mb-6 flex flex-col gap-2'>
+          <Link to='/trash'>Trash</Link>
+          <Link to='/settings'>Settings</Link>
+        </section>
+      </nav>
+    </div> : null
   );
 };
 
